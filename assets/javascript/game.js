be0826dd4a1514aca59dda3841fdb2ca59331eb5
Maeve McQ <maevemcqueeny6@gmail.com>
2019-04-02 20:57:10 -0400

    FINAL

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
 
 
 
-    var wins = 0;
-    var losses = 0;
-    var computerGuess;
-    console.log(computerGuess);
+    // var wins = 0;
+    // var losses = 0;
+    // var computerGuess;
+    // console.log(computerGuess);
     
-    function newRound() {
-        computerGuess = Math.floor(Math.random() * 120) + 19;
-        // $("#computerchoice-text").after(computerGuess);
-        console.log(computerGuess);
-    }
+    // function newRound() {
+    //     computerGuess = Math.floor(Math.random() * 120) + 19;
+    //     // $("#computerchoice-text").after(computerGuess);
+    //     console.log(computerGuess);
+    // }
     
-    newRound();
-    $("#computer-choicetext").append(computerGuess);
+    // newRound();
+    // $("#computer-choicetext").append(computerGuess);
 
     
     
