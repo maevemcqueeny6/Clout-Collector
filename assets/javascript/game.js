3d2a553de3e2eb09636b900ab03fa27d98bf8d5d
Maeve McQ <maevemcqueeny6@gmail.com>
2019-04-04 21:08:45 -0400

    Fixed result - FINAL

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,86 @@
+$("document").ready(function () {
 
 
 
-    // var wins = 0;
-    // var losses = 0;
-    // var computerGuess;
-    // console.log(computerGuess);
+    var wins = 0;
+    var losses = 0;
+    var result=0;
+    var computerGuess;
+    var rubyvalue;
+    var diamondvalue;
+    var emeraldvalue;
+    var cyanvalue;
+    var gamewon = false;
+    console.log(computerGuess);
     
-    // function newRound() {
-    //     computerGuess = Math.floor(Math.random() * 120) + 19;
-    //     // $("#computerchoice-text").after(computerGuess);
-    //     console.log(computerGuess);
-    // }
+    function newRound() {
+        computerGuess = Math.floor(Math.random() * 120) + 19;
+        console.log(computerGuess);
+        rubyvalue=Math.floor(Math.random() * 12) + 1;
+        diamondvalue=Math.floor(Math.random() * 12) + 1;
+        emeraldvalue=Math.floor(Math.random() * 12) + 1;
+        cyanvalue=Math.floor(Math.random() * 12) + 1;
+        result=0;
+        $("#computer-choicetext").text(computerGuess);
+        $("#result-text").empty();
+    }
     
-    // newRound();
-    // $("#computer-choicetext").append(computerGuess);
-
+    newRound();
+    
+    function check(){
+        if (computerGuess === result){
+        alert("You win!");
+         wins++;
+         $("#wins-text").text("Wins: " + wins);
+        newRound();
+    }
+    else if (result > computerGuess){
+        alert("You lose!");
+         losses++;
+         $("#losses-text").text("Losses: " + losses);
+        newRound();
+    }
+    }
+    
+    newRound();
     
     
-
+    $(".emerald").click(function(){
+        result= parseInt(result)+parseInt(emeraldvalue);
+        console.log(emeraldvalue);
+        console.log(result);
+        $("#result-text").html(result);
+        check();
+    })
+    
+    $(".diamond").click(function(){
+        result= parseInt(result)+parseInt(diamondvalue);
+        console.log(diamondvalue);
+        console.log(result);
+        $("#result-text").html(result);
+        check();
+    })
+    
+    $(".ruby").click(function(){
+        result= parseInt(result)+parseInt(rubyvalue);
+        console.log(rubyvalue);
+        console.log(result);
+        $("#result-text").html(result);
+        check();
+    })
+    
+    $(".cyan").click(function(){
+        result= parseInt(result)+parseInt(cyanvalue);
+        console.log(cyanvalue);
+        console.log(result);
+        $("#result-text").html(result);
+        check();
+    })
+    
+    
+    $("#result-text").append(result);
+    
+    
+    
+    
+    })
